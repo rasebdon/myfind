f5a09a34aae97a564ab88abc375a657fd4c4eaf8
Raphael Dohnalek <r.dohnalek@gmx.at>
2021-09-19 20:52:11 +0200

    fast compile and run added and started fiddling with multiprocessing

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(void)
 {
-    printf("Hello World!");
+    const int childAmount = 5;
+    pid_t childs[childAmount];
+    const long parentPId = (long)getpid();
+
+    for (int i = 0; i < childAmount; i++)
+    {
+        childs[i] = fork();
+
+        if (childs[i] == -1)
+        {
+            perror("Failed to fork");
+            return 1;
+        }
+
+        if (childs[i] == 0)
+        {
+            printf("I am child %ld \n", (long)getpid());
+            break;
+        }
+    }
+
+    sleep(4);
+
+    if((long)getpid() == parentPId)
+        printf("Parent done!\n");
     return 0;
 }
