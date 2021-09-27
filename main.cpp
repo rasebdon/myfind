99d0e9027ed2524d0cbf639fe095dbafd2e5c9f2
Raphael Dohnalek <r.dohnalek@gmx.at>
2021-09-27 13:55:32 +0200

    Added main.cpp header description

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
+/*
+    VERTS MyFind:
+    Finds specific files in the current directory and returns their absoute path
+    Usage:
+    ./myfind [-R] [-i] searchpath filename1 [filename2] ...[filenameN]
+    -R: 
+        should switch myfind in recursive mode 
+        (else the files should only be search in the defined folder) 
+    -i: 
+        case in-sensitive search 
+    searchpath:
+        can be an absolute or a relative path. 
+    filename:
+        only filenames as plain string 
+        no support for paths, subpaths, wildcards required. 
+    @file main.cpp
+    @author Michael Duskanich and Raphael Dohnalek
+    @version 1.0
+*/
 #include "messageQueue/msgQueue.h"
 #include "findAttributes.h"
 #include "findRootProcess.h"
 #include <signal.h>
 #include <sys/wait.h>
-// Plan:
-// Start server (reciever for findings)
-// Start client for every file
-// Send path to server if found
-// Stop server when all processes (count == 0) have been stopped 
 
 int main(int argc, char* argv[]) {
     // Get the user given search attributes
